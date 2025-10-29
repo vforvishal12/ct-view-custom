2ec15bf347de9b61a4290f272778e565562d94b9
vforvishal12 <52970035+vforvishal12@users.noreply.github.com>
2025-10-29 14:54:29 +0530

    Add files via upload

---
+++ custom-view-config.mjs
@@ -0,0 +1,27 @@
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomView}
+ */
+const config = {
+  name: 'My Ct Custom View',
+  cloudIdentifier: 'aws-eu',
+  env: {
+    development: {
+      initialProjectKey: 'ct-training-1',
+    },
+    production: {
+      customViewId: 'TODO',
+      url: 'https://my-custom-view.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products','view_orders'],
+    manage: ['manage_products','manage_orders'],
+  },
+  type: 'CustomPanel',
+  typeSettings: {
+    size: 'LARGE',
+  },
+  locators: ['products.product_details.general'],
+};
+
+export default config;
